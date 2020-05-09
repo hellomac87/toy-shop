706f1fef52e5e55df44fd01bb21e2748d7e7bd54
hellomac87 <hellomac87@gmail.com>
2020-05-09 22:01:49 +0900

    이미지 파일 서버로 보내기

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
+import axios from "axios";
 
 const FileUpload = () => {
+  const dropHanlder = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+
+    axios.post(`/api/product/image`, formData, config).then((res) => {
+      console.log(res);
+      if (res.data.success) {
+      } else {
+        alert("파일을 저장하는데 실패했습니다.");
+      }
+    });
+  };
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+      <Dropzone onDrop={dropHanlder}>
         {({ getRootProps, getInputProps }) => (
           <div
             style={{
