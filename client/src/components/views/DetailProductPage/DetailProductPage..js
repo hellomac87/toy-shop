2265851de69839210b698f312a551529bbf119ed
hellomac87 <hellomac87@gmail.com>
2020-05-24 22:02:11 +0900

    이미지 컴포넌트 만들기

--- client/src/components/views/DetailProductPage/DetailProductPage..js
+++ client/src/components/views/DetailProductPage/DetailProductPage..js
@@ -1,8 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ProductImage from "./Sections/ProductImage";
+import ProductInfo from "./Sections/ProductInfo";
+import { Row, Col } from "antd";
 
 function DetailProductPage({ match }) {
   const { productId } = match.params;
+
+  const [product, setProduct] = useState({});
 
   useEffect(() => {
     axios
@@ -10,12 +15,33 @@ function DetailProductPage({ match }) {
       .then((res) => {
         if (res.data.success) {
           console.log(res.data);
+          setProduct(res.data.product[0]);
         } else {
           alert("상세 정보 가져오기를 실패했습니다.");
         }
       });
   }, []);
-  return <div>DetailProductPage</div>;
+
+  return (
+    <div style={{ width: "100", padding: "3rem 4rem" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1>{product.title}</h1>
+      </div>
+
+      <br />
+
+      <Row gutter={[16, 16]}>
+        <Col lg={12} sm={24}>
+          {/* Product Image */}
+          <ProductImage detail={product} />
+        </Col>
+        <Col lg={12} sm={24}>
+          {/* Product Info */}
+          <ProductInfo />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default DetailProductPage;
---
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import ImageGallery from "react-image-gallery";
+// import "react-image-gallery/styles/css/image-gallery.css";
+
+function ProductImage({ detail }) {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    if (detail.images && detail.images.length > 0) {
+      let images = [];
+      detail.images.map((item) => {
+        images.push({
+          original: `http://localhost:5000/${item}`,
+          thumbnail: `http://localhost:5000/${item}`,
+        });
+      });
+      setImages(images);
+    }
+  }, [detail]);
+
+  return (
+    <div>
+      <ImageGallery items={images} />
+    </div>
+  );
+}
+
+export default ProductImage;
