92aaa76daa33c36caf9d98d31df6e69e760ee042
hellomac87 <hellomac87@gmail.com>
2020-06-10 23:10:25 +0900

    카트 상품 계산

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_actions";
 import UserCardBlock from "./Sections/UserCardBlock";
 function CartPage() {
+  const [total, setTotal] = useState(0);
   const { userData, cartDetail } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   // nope
@@ -14,15 +15,31 @@ function CartPage() {
           cartItem.push(item.id);
         });
 
-        dispatch(getCartItems(cartItem, userData.cart));
+        dispatch(getCartItems(cartItem, userData.cart)).then((response) => {
+          calculateTotal(response.payload);
+        });
       }
     }
   }, [dispatch, userData]);
+
+  const calculateTotal = (cartDetail) => {
+    let result = 0;
+
+    cartDetail.map((item) => {
+      result += parseInt(item.price) * item.quantity;
+    });
+
+    setTotal(result);
+  };
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
       <h1>My cart</h1>
       <div>
         <UserCardBlock products={cartDetail} />
+      </div>
+
+      <div style={{ marginTop: "3rem" }}>
+        <div>Total Price : {total}</div>
       </div>
     </div>
   );
